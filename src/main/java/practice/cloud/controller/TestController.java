7e646f0c631cde8f0adec5e03bd8a22b150dd525
jhl221123 <jhl221123@naver.com>
2023-09-19 17:44:05 +0900

    deamon 옵션 제거 후 빌드 속도 측정

--- src/main/java/practice/cloud/controller/TestController.java
+++ src/main/java/practice/cloud/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
     @GetMapping("/test")
     public void test() {
-        System.out.println("캐시 옵션 제거 후 빌드 속도 측정(2번째 시도)");
+        System.out.println("daemon 옵션 제거 후 빌드 속도 측정");
     }
 }
