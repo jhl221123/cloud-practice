e23b9105a0b4b0987b1947a4e19f792673008d65
jhl221123 <jhl221123@naver.com>
2023-09-19 16:40:16 +0900

    ci 캐시 옵션 적용 후 빌드 속도 측정

--- src/main/java/practice/cloud/controller/TestController.java
+++ src/main/java/practice/cloud/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
     @GetMapping("/test")
     public void test() {
-        System.out.println("actions/setup-java@v3: 캐시 옵션 사용 전 빌드 속도 측정");
+        System.out.println("actions/setup-java@v3: 캐시 옵션 사용 후 빌드 속도 측정");
     }
 }
